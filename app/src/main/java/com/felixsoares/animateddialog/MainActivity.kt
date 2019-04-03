8be8bb8ded44abc3b8cffc5b3fea894490a0eed5
Felix <felix.soares.ferreira@gmail.com>
2019-04-03 08:31:09 -0300

    commit

--- app/src/main/java/com/felixsoares/animateddialog/MainActivity.kt
+++ app/src/main/java/com/felixsoares/animateddialog/MainActivity.kt
@@ -11,24 +11,177 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtMessage.setOnClickListener {
+        btn1.setOnClickListener {
 
             SweetDialog()
-                .setTitle("Hello world!")
-                .setMessage("Lorem ypsuin log lgo gskdosi ndgpg sdnkgdgd gdsjgd")
-                .setAnimation(SweetDialog.Animation.BOUNCE)
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .show(this)
+        }
+
+        btn2.setOnClickListener {
+
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
                 .setCancellable(false)
-                .setTimer(5000)
-                .setPositiveColor(R.color.green)
                 .setPositiveText("Ok")
                 .onPositiveClick { dialog ->
                     dialog.dismiss()
                 }
-                .setNegativeColor(R.color.red)
+                .show(this)
+        }
+
+        btn3.setOnClickListener {
+
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setCancellable(false)
+                .setPositiveText("Ok")
+                .onPositiveClick { dialog ->
+                    dialog.dismiss()
+                }
                 .setNegativeText("Cancel")
                 .onNegativeClick { dialog ->
                     dialog.dismiss()
                 }
+                .show(this)
+        }
+
+        btn4.setOnClickListener {
+
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setCancellable(false)
+                .setPositiveColor(android.R.color.holo_orange_dark)
+                .setPositiveTextColor(R.color.black)
+                .setPositiveText("Positive =)")
+                .onPositiveClick { dialog ->
+                    dialog.dismiss()
+                }
+                .setNegativeColor(R.color.black)
+                .setNegativeTextColor(R.color.red)
+                .setNegativeText("Negative =(")
+                .onNegativeClick { dialog ->
+                    dialog.dismiss()
+                }
+                .show(this)
+        }
+
+        btn5.setOnClickListener {
+
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setCancellable(false)
+                .setTimer(5000)
+                .show(this)
+        }
+
+        btn6.setOnClickListener {
+
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setType(SweetDialog.Type.SUCCESS)
+                .show(this)
+        }
+
+        btn7.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setType(SweetDialog.Type.ERROR)
+                .show(this)
+        }
+
+        btn8.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setType(SweetDialog.Type.DANGER)
+                .show(this)
+        }
+
+        btn9.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.IN_TO_OUT)
+                .show(this)
+        }
+
+        btn10.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.BOUNCE)
+                .show(this)
+        }
+
+        btn11.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.LEFT_TO_LEFT)
+                .show(this)
+        }
+
+        btn12.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.LEFT_TO_RIGHT)
+                .show(this)
+        }
+
+        btn13.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.RIGTH_TO_RIGHT)
+                .show(this)
+        }
+
+        btn14.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.RIGHT_TO_LEFT)
+                .show(this)
+        }
+
+        btn15.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.BOTTOM_TO_BOTTOM)
+                .show(this)
+        }
+
+        btn16.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.BOTTOM_TO_TOP)
+                .show(this)
+        }
+
+        btn17.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.TOP_TO_TOP)
+                .show(this)
+        }
+
+        btn18.setOnClickListener {
+            SweetDialog()
+                .setTitle("Lorem ipsum")
+                .setMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna diam.")
+                .setAnimation(SweetDialog.Animation.TOP_TO_BOTTOM)
                 .show(this)
         }
     }
